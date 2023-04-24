5258171d5be5cdc8a1635ede6fe73095229db9ac
wiss <73744299+wissem18@users.noreply.github.com>
2023-04-24 12:28:50 +0100

    Time: 17 ms (48.19%), Space: 7.2 MB (85.19%) - LeetHub

---
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+int minDistance(string word1, string word2) {
+    int n = word1.size(), m = word2.size();
+    int dp[n + 1][m + 1];
+    dp[0][0] = 0;
+    for (int i = 1; i <= n; ++i) {
+        dp[i][0] = i;
+    }
+    for (int i = 1; i <= m; ++i) {
+        dp[0][i] = i;
+    }
+    for (int i = 1; i <=n ; ++i) {
+        for (int j = 1; j <=m ; ++j) {
+            dp[i][j]=min({1+dp[i-1][j],1+dp[i][j-1],dp[i-1][j-1]+(word1[i-1]!=word2[j-1])});
+        }
+    }
+    return dp[n][m];
+}
+};
