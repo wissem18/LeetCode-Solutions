7c3cb23ddc59319ea21bd5cfbafb07d7ffc8523c
wiss <73744299+wissem18@users.noreply.github.com>
2023-04-26 14:15:40 +0100

    Time: 1910 ms (6.98%), Space: 20.8 MB (19.71%) - LeetHub

---
+++ 0567-permutation-in-string/0567-permutation-in-string.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+ bool checkInclusion(string s1, string s2) {
+         if(s1.size()>s2.size())
+        return false;
+vector<int> occ(26,0);
+    for (int i = 0; i < s1.size(); ++i) {
+        occ[s1[i]-'a']++;
+    }
+    int i=0;
+    while(i<s2.size()){
+        vector<int> occ2(26,0);
+        int j=i;
+        int nb=0;
+        while(j<s2.size()&&occ2[s2[j]-'a']<occ[s2[j]-'a']&&occ[s2[j]-'a']){
+            occ2[s2[j]-'a']++;
+            j++;
+            nb++;
+            if(nb==s1.size())
+                return true;
+        }
+       i++;
+    }
+    return false;
+}
+};
