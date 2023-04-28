28e6369c2c0422c4e9c9f25decfd270c90dc6026
wiss <73744299+wissem18@users.noreply.github.com>
2023-04-28 13:10:28 +0100

    Time: 35 ms (76.19%), Space: 10.1 MB (91.43%) - LeetHub

---
+++ 0839-similar-string-groups/0839-similar-string-groups.cpp
@@ -0,0 +1,54 @@
+class Solution {
+public:
+bool similair(string &a,string &b){
+    int cnt=0;
+    for (int i = 0; i < a.size(); ++i) {
+        if(a[i]!=b[i])
+            cnt++;
+        if(cnt>2)
+            return false;
+    }
+   return true;
+}
+vector<int> parent;
+vector<int> sz;
+void make_set(int v) {
+    parent[v] = v;
+    sz[v] = 1;
+}
+int find_set(int v) {
+    if (v == parent[v])
+        return v;
+    return parent[v] = find_set(parent[v]);
+}
+
+void union_sets(int a, int b) {
+    a = find_set(a);
+    b = find_set(b);
+    if (a != b) {
+        if (sz[a] < sz[b])
+            swap(a, b);
+        parent[b] = a;
+        sz[a] += sz[b];
+    }
+}
+    int numSimilarGroups(vector<string>& strs) {
+    int ans=strs.size();
+       parent.resize(ans);
+    sz.resize(ans);
+        for (int i = 0; i < ans; ++i) {
+            make_set(i);
+        }
+        for (int i = 0; i < strs.size(); ++i) {
+            for (int j = i+1; j <strs.size(); ++j) {
+             if(find_set(i)!=find_set(j)){
+                 if(similair(strs[i],strs[j])){
+                     ans--;
+                     union_sets(i,j);
+                 }
+             }
+            }
+        }
+        return ans;
+    }
+};
