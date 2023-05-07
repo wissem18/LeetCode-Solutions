f9be88534744785e068e15e1998d595e8b383202
wiss <73744299+wissem18@users.noreply.github.com>
2023-05-07 12:42:12 +0100

    Time: 0 ms (100.00%), Space: 6 MB (32.23%) - LeetHub

---
+++ 2544-alternating-digit-sum/2544-alternating-digit-sum.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int alternateDigitSum(int n) {
+string s= to_string(n);
+int ans=0;
+    for (int i = 0; i < s.size(); ++i) {
+        if(i%2)
+            ans-=(s[i]-'0');
+        else
+            ans+=(s[i]-'0');
+    }
+    return ans;
+}
+};
