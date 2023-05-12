8585913ca15654b67f00d2f54b90c45d186830b3
wiss <73744299+wissem18@users.noreply.github.com>
2023-05-12 10:36:03 +0100

    Time: 400 ms (24.27%), Space: 121.4 MB (53.71%) - LeetHub

---
+++ 2140-solving-questions-with-brainpower/2140-solving-questions-with-brainpower.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+int n;
+vector<long long>mem;
+long long dp(int i,vector<vector<int>>& questions){
+    if(i>=n)
+    {
+        return 0;
+    }
+    if (mem[i]!=-1){
+        return mem[i];
+    }
+    long long ans=0;
+   ans=ans+max(questions[i][0]+dp(i+1+questions[i][1],questions),dp(i+1,questions));
+    return mem[i]=ans;
+}
+long long mostPoints(vector<vector<int>>& questions) {
+n=questions.size();
+mem.assign(n,-1);
+return dp(0,questions);
+}
+};
