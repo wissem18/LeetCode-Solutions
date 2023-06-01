3ec556b2173a816935fb20aa840396fe16fd031b
wiss <73744299+wissem18@users.noreply.github.com>
2023-06-01 21:48:06 +0100

    Time: 59 ms (63.84%), Space: 42.3 MB (92.83%) - LeetHub

---
+++ 2695-array-wrapper/2695-array-wrapper.js
@@ -0,0 +1,25 @@
+/**
+ * @param {number[]} nums
+ */
+var ArrayWrapper = function(nums) {
+    this.nums=nums;
+};
+
+ArrayWrapper.prototype.valueOf = function() {
+    let ans=0;
+    for(let i=0;i<this.nums.length;i++)
+        ans+=this.nums[i];
+    return ans;
+}
+
+ArrayWrapper.prototype.toString = function() {
+    return JSON.stringify(this.nums);
+}
+
+/**
+ * const obj1 = new ArrayWrapper([1,2]);
+ * const obj2 = new ArrayWrapper([3,4]);
+ * obj1 + obj2; // 10
+ * String(obj1); // "[1,2]"
+ * String(obj2); // "[3,4]"
+ */
