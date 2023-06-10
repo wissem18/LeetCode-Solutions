d3a9b5a51f84eeee3552e8f0df68aa88f9e014b1
wiss <73744299+wissem18@users.noreply.github.com>
2023-06-10 18:42:12 +0100

    Time: 196 ms (40.00%), Space: 93.2 MB (40.00%) - LeetHub

--- 2731-movement-of-robots/2731-movement-of-robots.cpp
+++ 2731-movement-of-robots/2731-movement-of-robots.cpp
@@ -2,20 +2,19 @@ class Solution {
 public:
 int MOD=1000000007;
 int sumDistance(vector<int>& nums, string s, int d) {
-vector<long long>a;
 int n=nums.size();
     for (int i = 0; i < n; ++i) {
         if(s[i]=='R'){
-            a.push_back(nums[i]+d);
+          nums[i]=nums[i]+d;
         }
         else{
-            a.push_back(nums[i]-d);
+            nums[i]=nums[i]-d;
         }
     }
-    sort(a.rbegin(),a.rend());
+    sort(nums.rbegin(),nums.rend());
     int ans=0;
     for (int i = 0; i < n; ++i) {
-        ans=(ans%MOD+(a[i]*1LL*(n-2*i-1))%MOD+MOD)%MOD;
+        ans=(ans%MOD+(nums[i]*1LL*(n-2*i-1))%MOD+MOD)%MOD;
     }
     
     return ans;
