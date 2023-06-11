22e3d5ceb98c17d6948fe0a01910cc7199123d4f
wiss <73744299+wissem18@users.noreply.github.com>
2023-06-11 21:21:00 +0100

    Time: 100 ms (66.67%), Space: 40.2 MB (16.67%) - LeetHub

---
+++ 2734-lexicographically-smallest-string-after-substring-operation/2734-lexicographically-smallest-string-after-substring-operation.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    string smallestString(string s) {
+int i=0;
+int n=s.size();
+while(i<n&&s[i]=='a')i++;
+if(i==n){
+    s[n-1]='z';
+}        
+while(i<n&&s[i]!='a'){
+    s[i]--;
+    i++;
+}
+return s;
+}
+};
