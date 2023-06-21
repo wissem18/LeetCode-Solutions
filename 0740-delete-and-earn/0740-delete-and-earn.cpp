35a81402fd6c71b681f0fe869326d92e1d04df59
wiss <73744299+wissem18@users.noreply.github.com>
2023-06-21 13:49:22 +0100

    Time: 12 ms (80.84%), Space: 12.1 MB (79.04%) - LeetHub

---
+++ 0740-delete-and-earn/0740-delete-and-earn.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+   vector<int>mem;
+int dp(int i,vector<int>&a){
+    if(i>=a.size())
+        return 0;
+    if(mem[i]!=-1)
+     return mem[i];
+    return mem[i]=max(dp(i+2,a)+a[i]*i,dp(i+1,a));
+}
+int deleteAndEarn(vector<int>& nums) {
+int mx=*max_element(nums.begin(),nums.end());
+vector<int>a(mx+1,0);
+    for (int i = 0; i < nums.size(); ++i) {
+        a[nums[i]]++;
+    }
+    mem.assign(mx+1,-1);
+    return dp(1,a);
+}
+};
