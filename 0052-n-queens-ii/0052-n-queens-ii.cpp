0904fe309e050f0c1f5aad6b1cc6e3d504d6aa40
wiss <73744299+wissem18@users.noreply.github.com>
2023-08-05 23:11:28 +0100

    Time: 3 ms (85.77%), Space: 6.2 MB (46.11%) - LeetHub

--- 0052-n-queens-ii/0052-n-queens-ii.cpp
+++ 0052-n-queens-ii/0052-n-queens-ii.cpp
@@ -2,19 +2,17 @@ class Solution {
 public:
 vector<bool> col, diag1, diag2;
 
-int rec(int i, int n, vector<string> cur) {
+int rec(int i, int &n) {
     if (i == n) {
         return 1;
     }
     int res=0;
     for (int j = 0; j < n; ++j) {
         if (!col[j] && !diag1[j - i + n - 1] && !diag2[i + j]) {
-            cur[i][j] = 'Q';
             col[j] = true;
             diag1[j - i + n - 1] = true;
             diag2[i + j] = true;
-            res+=rec(i + 1, n, cur);
-            cur[i][j] = '.';
+            res+=rec(i + 1, n);
             col[j] = false;
             diag1[j - i + n - 1] = false;
             diag2[i + j] = false;
@@ -27,12 +25,6 @@ int totalNQueens(int n) {
     col.assign(n, false);
     diag1.assign(2 * n, false);
     diag2.assign(2 * n, false);
-    vector<string> cur(n);
-    for (int i = 0; i < n; ++i) {
-        for (int j = 0; j < n; ++j) {
-            cur[i].push_back('.');
-        }
-    }
-    return rec(0, n, cur);
+    return rec(0, n);
 }
 };
