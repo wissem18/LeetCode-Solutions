da64e765c0259daa82523d4022e2295706276ca6
wiss <73744299+wissem18@users.noreply.github.com>
2023-09-22 12:24:55 +0100

    Time: 3 ms (49.10%), Space: 6.9 MB (10.03%) - LeetHub

---
+++ 0392-is-subsequence/0392-is-subsequence.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool isSubsequence(string s, string t) {
+        int n = s.length(), m = t.length();
+int i = 0, j = 0;
+while (i < n && j < m) {
+if (s[i] == t[j])
+i++;
+j++;
+}
+/*If i reaches end of s1,that mean we found all
+characters of s1 in s2,
+so s1 is subsequence of s2, else not*/
+return i == n;
+    }
+};
