fa932a2e9d7e74e6804b1e45b4b2b2ed6a8ba7f9
wiss <73744299+wissem18@users.noreply.github.com>
2023-09-30 19:12:34 +0100

    Time: 137 ms (16.67%), Space: 85.4 MB (16.67%) - LeetHub

---
+++ 2870-minimum-number-of-operations-to-make-array-empty/2870-minimum-number-of-operations-to-make-array-empty.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+int minOperations(vector<int>& nums) {
+    int n=nums.size();
+    map<int,int>cnt;
+    int ans=0;
+    for (int i = 0; i < n; ++i) {
+        cnt[nums[i]]++;
+    }
+    for(auto x:cnt){
+      if(x.second==1)
+          return -1;
+      ans=ans+(x.second+2)/3;
+    }
+    return ans;
+}
+};
