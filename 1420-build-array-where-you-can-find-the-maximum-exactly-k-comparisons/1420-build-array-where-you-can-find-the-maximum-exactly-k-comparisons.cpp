7932a2bc6edd7debf88fd6eab22546b31a52a240
wiss <73744299+wissem18@users.noreply.github.com>
2023-10-07 11:49:39 +0100

    Time: 42 ms (72.81%), Space: 7.6 MB (61.75%) - LeetHub

--- 1420-build-array-where-you-can-find-the-maximum-exactly-k-comparisons/1420-build-array-where-you-can-find-the-maximum-exactly-k-comparisons.cpp
+++ 1420-build-array-where-you-can-find-the-maximum-exactly-k-comparisons/1420-build-array-where-you-can-find-the-maximum-exactly-k-comparisons.cpp
@@ -3,14 +3,13 @@ public:
     int MOD = 1e9 + 7;
 int mem[50][101][51];
 int dp(int i,int mx,int cost,int &n,int &m,int &k){
+    if(cost>k)
+        return 0;
     if(i==n)
         return cost==k;
     if(mem[i][mx][cost]!=-1)
         return mem[i][mx][cost];
-    int ans=0;
-    for (int j = 1; j <=mx ; ++j) {
-        ans=(ans+dp(i+1,mx, cost,n,m,k))%MOD;
-    }
+    int ans=(dp(i+1,mx, cost,n,m,k)*1LL*mx)%MOD;
     for (int j = mx+1; j <=m ; ++j) {
         ans=(ans+dp(i+1,j, cost+1,n,m,k))%MOD;
     }
