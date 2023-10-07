431ff1aceebbe49cc9b679e20468853a76b89949
wiss <73744299+wissem18@users.noreply.github.com>
2023-10-07 11:59:11 +0100

    Time: 39 ms (75.58%), Space: 7.4 MB (64.52%) - LeetHub

--- 1420-build-array-where-you-can-find-the-maximum-exactly-k-comparisons/1420-build-array-where-you-can-find-the-maximum-exactly-k-comparisons.cpp
+++ 1420-build-array-where-you-can-find-the-maximum-exactly-k-comparisons/1420-build-array-where-you-can-find-the-maximum-exactly-k-comparisons.cpp
@@ -1,22 +1,22 @@
 class Solution {
 public:
-    int MOD = 1e9 + 7;
+int MOD = 1e9 + 7;
 int mem[50][101][51];
-int dp(int i,int mx,int cost,int &n,int &m,int &k){
-    if(cost>k)
+int dp(int i,int mx,int cost,int &m,int &n){
+    if(cost<0)
         return 0;
     if(i==n)
-        return cost==k;
+        return (cost==0);
     if(mem[i][mx][cost]!=-1)
         return mem[i][mx][cost];
-    int ans=(dp(i+1,mx, cost,n,m,k)*1LL*mx)%MOD;
+    int ans=(dp(i+1,mx, cost,m,n)*1LL*mx)%MOD;
     for (int j = mx+1; j <=m ; ++j) {
-        ans=(ans+dp(i+1,j, cost+1,n,m,k))%MOD;
+        ans=(ans+dp(i+1,j, cost-1,m,n))%MOD;
     }
     return mem[i][mx][cost]=ans;
 }
 int numOfArrays(int n, int m, int k) {
     memset(mem,-1,sizeof mem);
-return dp(0,0,0,n,m,k);
+return dp(0,0,k,m,n);
 }
 };
