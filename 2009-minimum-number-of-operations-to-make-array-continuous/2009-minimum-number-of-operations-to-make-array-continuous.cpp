d014127636765321804ab91eb4a75735a0c4183f
wiss <73744299+wissem18@users.noreply.github.com>
2023-10-10 12:21:51 +0100

    Time: 211 ms (50.21%), Space: 67.1 MB (65.06%) - LeetHub

--- 2009-minimum-number-of-operations-to-make-array-continuous/2009-minimum-number-of-operations-to-make-array-continuous.cpp
+++ 2009-minimum-number-of-operations-to-make-array-continuous/2009-minimum-number-of-operations-to-make-array-continuous.cpp
@@ -10,8 +10,7 @@ vector<int> duplcount(n+1,0);
 int ans=1e9;
     for (int i = 0; i < nums.size(); ++i) {
         int x= upper_bound(nums.begin(),nums.end(),nums[i]+n-1)-nums.begin();
-        x--;
-        ans=min(ans,n-x+i-1+duplcount[x+1]-duplcount[i]);
+        ans=min(ans,n-x+i+duplcount[x]-duplcount[i]);
     }
     return ans;
 }
