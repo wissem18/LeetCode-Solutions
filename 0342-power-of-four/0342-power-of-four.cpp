ad1a6fde448bfa8f3fc413fadb53a7f3057b5c89
wiss <73744299+wissem18@users.noreply.github.com>
2023-10-23 20:07:18 +0100

    Time: 0 ms (100.00%), Space: 6.3 MB (47.70%) - LeetHub

--- 0342-power-of-four/0342-power-of-four.cpp
+++ 0342-power-of-four/0342-power-of-four.cpp
@@ -1,15 +1,6 @@
 class Solution {
 public:
     bool isPowerOfFour(int n) {
-        if(n==1)
-            return true;
-        if(n<=0)
-            return false;
-        double x=log(n)/log(4);
-        int y=log(n)/log(4);
-      if(x==y)
-          return true;
-        else
-            return false;
+    return(n>0&&(n&(n-1))==0 && (__builtin_ctz(n)&1)==0);
     }
 };
