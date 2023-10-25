5a4046e64bcc4b4212986b1844ae02c42feb2c15
wiss <73744299+wissem18@users.noreply.github.com>
2023-10-25 17:24:09 +0100

    Time: 2 ms (51.37%), Space: 6.2 MB (53.37%) - LeetHub

--- 0779-k-th-symbol-in-grammar/0779-k-th-symbol-in-grammar.cpp
+++ 0779-k-th-symbol-in-grammar/0779-k-th-symbol-in-grammar.cpp
@@ -1,16 +1,16 @@
 class Solution {
 public:
-int rec(int n, int k, int x) {
+int rec(int n, int k) {
     if (n == 0) {
-        return x;
+        return 0;
     }
     if (k <= (1 << (n - 1)))
-        return rec(n - 1, k, x);
+        return rec(n - 1, k);
     else
-        return rec(n-1,k-(1<<(n-1)),!x);
+        return !rec(n-1,k-(1<<(n-1)));
 }
 
 int kthGrammar(int n, int k) {
-    return rec(n-1,k,0);
+    return rec(n-1,k);
 }
 };
