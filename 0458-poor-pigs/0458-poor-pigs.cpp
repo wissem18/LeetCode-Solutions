fd3e514ab1fe08e65e750a3a7312bf11c95a17ac
wiss <73744299+wissem18@users.noreply.github.com>
2023-10-29 10:56:02 +0100

    Time: 2 ms (53.09%), Space: 6.5 MB (39.09%) - LeetHub

--- 0458-poor-pigs/0458-poor-pigs.cpp
+++ 0458-poor-pigs/0458-poor-pigs.cpp
@@ -2,12 +2,14 @@ class Solution {
 public:
 int poorPigs(int buckets, int minutesToDie, int minutesToTest) {
 int nbOfTests=minutesToTest/minutesToDie;
-int ans=0;
-    for (int i = 0; i < 15; ++i) {
-        if(pow(nbOfTests+1,ans)>=buckets)
-            break;
-        ans++;
-    }
-    return ans;
+int l=0,r=15;
+while(l<r){
+    int mid=(l+r)/2;
+    if(pow(nbOfTests+1,mid)>=buckets)
+        r=mid;
+    else
+        l=mid+1;
+}
+    return l;
 }
 };
