8e36b20acb34a45b8d153b339b45f1f2f15bf78e
wiss <73744299+wissem18@users.noreply.github.com>
2023-11-18 12:46:17 +0100

    Time: 297 ms (5.15%), Space: 107.9 MB (7.15%) - LeetHub

---
+++ 1838-frequency-of-the-most-frequent-element/1838-frequency-of-the-most-frequent-element.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+int maxFrequency(vector<int> &nums, int k) {
+    int n = nums.size();
+    sort(nums.begin(), nums.end());
+    vector<long long>pre(n+1,0);
+    for (int i = 0; i < n; ++i) {
+        pre[i+1]=pre[i]+nums[i];
+    }
+    int ans=1;
+    for (int i =n-1; i >=0; --i) {
+      int l=0,r=i;
+      while(l<r){
+          int mid=(l+r)/2;
+          long long cur=pre[i]-pre[mid];
+          if(nums[i]*1LL*(i-mid)-cur>k){
+              l=mid+1;
+          }
+          else
+              r=mid;
+      }
+      ans=max(ans,i-l+1);
+    }
+    return ans;
+}
+};
