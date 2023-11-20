cfe879d07c074833fb57ea375a72ce2cdb267884
wiss <73744299+wissem18@users.noreply.github.com>
2023-11-20 22:04:14 +0100

    Time: 174 ms (43.61%), Space: 104.2 MB (8.08%) - LeetHub

---
+++ 2391-minimum-amount-of-time-to-collect-garbage/2391-minimum-amount-of-time-to-collect-garbage.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+int garbageCollection(vector<string> &garbage, vector<int> &travel) {
+    map<char, int> lst;
+    int n = travel.size();
+    int ans = 0;
+    for (int i = 0; i < garbage.size(); ++i) {
+        for (int j = 0; j < garbage[i].size(); ++j) {
+            ans++;
+            lst[garbage[i][j]] = i;
+        }
+    }
+    vector<int> pre(n + 1, 0);
+    for (int i = 0; i < n; ++i) {
+        pre[i + 1] = pre[i] + travel[i];
+    }
+
+    for (auto x: lst) {
+        ans += pre[x.second];
+    }
+    return ans;
+}
+    
+};
