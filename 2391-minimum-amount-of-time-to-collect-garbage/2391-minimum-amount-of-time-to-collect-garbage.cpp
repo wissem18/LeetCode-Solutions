87fd8d304ded95f00e69d7b951b99794c2a47440
wiss <73744299+wissem18@users.noreply.github.com>
2023-11-20 22:07:43 +0100

    Time: 195 ms (14.13%), Space: 103.9 MB (10.77%) - LeetHub

--- 2391-minimum-amount-of-time-to-collect-garbage/2391-minimum-amount-of-time-to-collect-garbage.cpp
+++ 2391-minimum-amount-of-time-to-collect-garbage/2391-minimum-amount-of-time-to-collect-garbage.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
 int garbageCollection(vector<string> &garbage, vector<int> &travel) {
-    map<char, int> lst;
+    unordered_map<char, int> lst;
     int n = travel.size();
     int ans = 0;
     for (int i = 0; i < garbage.size(); ++i) {
