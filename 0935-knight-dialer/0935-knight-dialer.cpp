dd746e9a72234775ed8fb84e07819dd5f7241499
wiss <73744299+wissem18@users.noreply.github.com>
2023-11-27 10:48:47 +0100

    Time: 3 ms (97.66%), Space: 8.2 MB (72.51%) - LeetHub

--- 0935-knight-dialer/0935-knight-dialer.cpp
+++ 0935-knight-dialer/0935-knight-dialer.cpp
@@ -1,25 +1,17 @@
 class Solution {
 public:
-int MOD = 1e9+7;
-vector<vector<int>>mem;
-vector<int>adj[10]={{4,6},{6,8},{7,9},{4,8},{0,3,9},{},{0,1,7},{2,6},{1,3},{2,4}};
-int dp(int i ,int cur,int &n){
-    if(i==n-1)
-        return 1;
-    if(mem[i][cur]!=-1)
-        return mem[i][cur];
-    int ans=0;
-    for (auto x:adj[cur]) {
-        ans=(ans+dp(i+1,x,n))%MOD;
-    }
-    return mem[i][cur]=ans;
+    int MOD = 1e9+7;
+int first[5]={	10, 20, 46, 104, 240};
+vector<int>mem;
+int dp(int i,int &n){
+    if(i<5)
+        return first[i];
+    if(mem[i]!=-1)
+        return mem[i];
+    return mem[i]=(((6LL*dp(i-2,n))%MOD-4LL*dp(i-4,n))%MOD+MOD)%MOD;
 }
 int knightDialer(int n) {
-    int ans=0;
-    mem.assign(n,vector<int>(10,-1));
-    for(int i=0;i<10;i++){
-        ans=(ans+dp(0,i,n))%MOD;
-    }
-    return ans;
+mem.assign(n,-1);
+return dp(n-1,n);
 }
 };
