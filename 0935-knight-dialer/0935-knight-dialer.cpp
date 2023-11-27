f944f9d1663dd013242382c94cc412a8f86c33cf
wiss <73744299+wissem18@users.noreply.github.com>
2023-11-27 10:49:43 +0100

    Time: 3 ms (97.66%), Space: 8.2 MB (72.51%) - LeetHub

--- 0935-knight-dialer/0935-knight-dialer.cpp
+++ 0935-knight-dialer/0935-knight-dialer.cpp
@@ -3,15 +3,15 @@ public:
     int MOD = 1e9+7;
 int first[5]={	10, 20, 46, 104, 240};
 vector<int>mem;
-int dp(int i,int &n){
+int dp(int i){
     if(i<5)
         return first[i];
     if(mem[i]!=-1)
         return mem[i];
-    return mem[i]=(((6LL*dp(i-2,n))%MOD-4LL*dp(i-4,n))%MOD+MOD)%MOD;
+    return mem[i]=(((6LL*dp(i-2))%MOD-4LL*dp(i-4))%MOD+MOD)%MOD;
 }
 int knightDialer(int n) {
 mem.assign(n,-1);
-return dp(n-1,n);
+return dp(n-1);
 }
 };
