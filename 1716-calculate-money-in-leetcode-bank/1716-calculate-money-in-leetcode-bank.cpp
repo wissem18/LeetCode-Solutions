ea041628535d0d717c86bae091a6155e31a30a92
wiss <73744299+wissem18@users.noreply.github.com>
2023-12-06 10:25:04 +0100

    Time: 2 ms (49.74%), Space: 6.3 MB (70.53%) - LeetHub

--- 1716-calculate-money-in-leetcode-bank/1716-calculate-money-in-leetcode-bank.cpp
+++ 1716-calculate-money-in-leetcode-bank/1716-calculate-money-in-leetcode-bank.cpp
@@ -1,8 +1,6 @@
 class Solution {
 public:
 int totalMoney(int n) {
-int x=n/7;
-int rem=n%7;
-return (7*x*(7+x)/2)+(rem*(2*x+1+rem)/2);
+return (7*(n/7)*(7+(n/7))/2)+((n%7)*(2*(n/7)+1+(n%7))/2);
 }
 };
