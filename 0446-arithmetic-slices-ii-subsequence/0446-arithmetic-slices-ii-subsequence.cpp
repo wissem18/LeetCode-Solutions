e6fdf52bbf9ad1fc40d176919b85504a1094f8cb
wiss <73744299+wissem18@users.noreply.github.com>
2024-01-07 10:57:00 +0100

    Time: 530 ms (58.89%), Space: 154.3 MB (56.67%) - LeetHub

--- 0446-arithmetic-slices-ii-subsequence/0446-arithmetic-slices-ii-subsequence.cpp
+++ 0446-arithmetic-slices-ii-subsequence/0446-arithmetic-slices-ii-subsequence.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
 int numberOfArithmeticSlices(vector<int>& nums) {
-map<long long ,int>m[nums.size()];
+unordered_map<long long ,int>m[nums.size()];
 int ans=0;
     for (int i = 0; i < nums.size(); ++i) {
         for (int j = 0; j < i; ++j) {
