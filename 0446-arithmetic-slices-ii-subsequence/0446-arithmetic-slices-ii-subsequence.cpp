66588be812cf527663f04e2facf9de47bec59c35
wiss <73744299+wissem18@users.noreply.github.com>
2024-01-07 10:56:41 +0100

    Time: 712 ms (43.89%), Space: 134.9 MB (69.44%) - LeetHub

--- 0446-arithmetic-slices-ii-subsequence/0446-arithmetic-slices-ii-subsequence.cpp
+++ 0446-arithmetic-slices-ii-subsequence/0446-arithmetic-slices-ii-subsequence.cpp
@@ -1,14 +1,14 @@
 class Solution {
 public:
 int numberOfArithmeticSlices(vector<int>& nums) {
-unordered_map<long long ,int>m[nums.size()];
+map<long long ,int>m[nums.size()];
 int ans=0;
     for (int i = 0; i < nums.size(); ++i) {
         for (int j = 0; j < i; ++j) {
             long long dif=(long long)nums[i]-nums[j];
             int cnt=m[j].count(dif)?m[j][dif]:0;//the number of sequences of the same dif ending with j
             ans+=cnt;
-            m[i][dif]+=cnt+1;
+            m[i][dif]+=cnt+1;//the +1 is for the new sequence {j,i} and cnt of the old sequences that we add i in them 
         }
     }
     return ans;
