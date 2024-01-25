392445d022252b1eb319e494297ccaa885251c2c
wiss <73744299+wissem18@users.noreply.github.com>
2024-01-25 10:26:36 +0100

    Time: 51 ms (50.86%), Space: 25.8 MB (5.18%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -11,7 +11,7 @@ int dp(int i, int j, string &s1, string &s2) {
     if (s1[i] == s2[j]) {
         return mem[i][j] = 1 + dp(i + 1, j + 1, s1, s2);
     }
-    return mem[i][j] = max({dp(i + 1, j, s1, s2), dp(i, j + 1, s1, s2), dp(i + 1, j + 1, s1, s2)});
+    return mem[i][j] = max(dp(i + 1, j, s1, s2), dp(i, j + 1, s1, s2));
 }
 
 int longestCommonSubsequence(string text1, string text2) {
