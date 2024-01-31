752022c0d56218d473652fd21e06cdfa218daf50
wiss <73744299+wissem18@users.noreply.github.com>
2024-01-31 12:01:46 +0100

    Time: 121 ms (89.61%), Space: 100.9 MB (67.32%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.cpp
+++ 0739-daily-temperatures/0739-daily-temperatures.cpp
@@ -1,24 +1,15 @@
 class Solution {
 public:
-vector<int> dailyTemperatures(vector<int>& temperatures) {
-vector<vector<int>>pos(101);
-    for (int i = 0; i < temperatures.size(); ++i) {
-        pos[temperatures[i]].push_back(i);
-    }
-    vector<int>ans;
-    for (int i = 0; i < temperatures.size(); ++i) {
-        int x=temperatures[i];
-        int cur=1e9;
-        for (int j = x+1; j <=100 ; ++j) {
-            int ind= upper_bound(pos[j].begin(),pos[j].end(),i)-pos[j].begin();
-            if(ind!=pos[j].size()){
-                cur=min(cur,pos[j][ind]);
-            }
-        }
-        if(cur==1e9)
-            ans.push_back(0);
-        else
-            ans.push_back(cur-i);
+vector<int> dailyTemperatures(vector<int> &temperatures) {
+    int n = temperatures.size();
+    vector<int> ans(n, 0);
+    stack<int> s;
+    for (int i = n - 1; i >= 0; --i) {
+        while (!s.empty() && temperatures[s.top()] <= temperatures[i])s.pop();
+        if (!s.empty())
+            ans[i] = s.top() - i;
+        s.push(i);
+
     }
     return ans;
 }
