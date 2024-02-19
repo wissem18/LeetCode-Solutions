830a8d45c1c3634822ff1e045ec7fdf1ddade790
wiss <73744299+wissem18@users.noreply.github.com>
2024-02-19 10:38:51 +0100

    Time: 0 ms (100.00%), Space: 7.2 MB (19.05%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.cpp
+++ 0231-power-of-two/0231-power-of-two.cpp
@@ -3,9 +3,6 @@ public:
     bool isPowerOfTwo(int n) {
         if(n<=0)
             return false;
-        int x=log2(n);
-        if(n==pow(2,x))
-            return true;
-        return false;
+       return ((n&(n-1))==0);
     }
 };
