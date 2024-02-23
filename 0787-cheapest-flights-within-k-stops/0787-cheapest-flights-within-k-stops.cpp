02f4972729ca0f1c26872506bd73fa2c33f3b8e9
wiss <73744299+wissem18@users.noreply.github.com>
2024-02-23 18:48:07 +0100

    Time: 16 ms (69.71%), Space: 16.7 MB (49.98%) - LeetHub

--- 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.cpp
+++ 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.cpp
@@ -5,18 +5,19 @@ int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int
     q.push({src,0,0});
     vector<int>dist(n,1e9);
     dist[src]=0;
-
+    vector<vector<pair<int,int>>>flight(n);
+    for (int i = 0; i < flights.size(); ++i) {
+        flight[flights[i][0]].push_back({flights[i][1],flights[i][2]});
+    }
     while (!q.empty()){
         vector<int> parent = q.front();
         q.pop();
         if(parent[2]>k)
             continue;
-        for (int i = 0; i < flights.size(); ++i) {
-            if(flights[i][0]!=parent[0])
-                continue;
-            int next=flights[i][1];
-            if(parent[1]+flights[i][2]<dist[next]){
-                dist[next]=parent[1]+flights[i][2];
+        for (int i = 0; i < flight[parent[0]].size(); ++i) {
+            int next=flight[parent[0]][i].first;
+            if(parent[1]+flight[parent[0]][i].second<dist[next]){
+                dist[next]=parent[1]+flight[parent[0]][i].second;
                 q.push({next,dist[next],parent[2]+1});
             }
         }
