b519dac6c76fe2709c1d0596af454d11fc6825df
wiss <73744299+wissem18@users.noreply.github.com>
2024-03-26 15:58:57 +0100

    Time: 48 ms (39.24%), Space: 43.6 MB (81.44%) - LeetHub

--- 0041-first-missing-positive/0041-first-missing-positive.cpp
+++ 0041-first-missing-positive/0041-first-missing-positive.cpp
@@ -1,12 +1,26 @@
 class Solution {
 public:
-int firstMissingPositive(vector<int>& nums) {
-sort(nums.begin(),nums.end());
-int ans=1;
-    for (int i = 0; i < nums.size(); ++i) {
-        if(nums[i]<=0)continue;
-        if(nums[i]==ans)ans++;
+int firstMissingPositive(vector<int> &nums) {
+    int n = nums.size();
+    bool mex_is_one=true;
+    for (int i = 0; i < n; ++i) {
+        if(nums[i]==1)
+            mex_is_one=false;
+        if(nums[i]<=0||nums[i]>n){
+            nums[i]=1;
+        }
     }
-    return ans;
+    if(mex_is_one)
+        return 1;
+    for (int i = 0; i < n; ++i) {
+        nums[nums[i]%(n)] +=n;
+    }
+    for (int i = 2; i < n; ++i) {
+        if((nums[i]/n)<(1+(nums[i]%n==0)))
+            return i;
+    }
+     if((nums[0]/n)<(1+(nums[0]%n==0)))
+         return n;
+     return n+1;
 }
 };
