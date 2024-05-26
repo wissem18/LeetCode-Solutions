d623bf99e3e5810bd8ef837038e58540c19d663f
wiss <73744299+wissem18@users.noreply.github.com>
2024-05-26 12:12:39 +0100

    Time: 1301 ms (14.43%), Space: 402.5 MB (27.64%) - LeetHub

--- 0552-student-attendance-record-ii/0552-student-attendance-record-ii.cpp
+++ 0552-student-attendance-record-ii/0552-student-attendance-record-ii.cpp
@@ -3,23 +3,23 @@ public:
 long long MOD = 1e9 + 7;
 vector<vector<vector<int>>> mem;
 
-int dp(int i, int totA, int conL, int &n) {
+int dp(int i, int totA, int conL) {
     if (totA > 1)
         return 0;
     if (conL > 2)
         return 0;
-    if (i == n) {
+    if (i == 0) {
         return 1;
     }
     if (mem[i][totA][conL] != -1)
         return mem[i][totA][conL];
-    int ans = ((dp(i + 1, totA, 0, n) % MOD + dp(i + 1, totA + 1, 0, n) % MOD) % MOD +
-               dp(i + 1, totA, conL + 1, n) % MOD) % MOD;
+    int ans = ((dp(i - 1, totA, 0) % MOD + dp(i - 1, totA + 1, 0) % MOD) % MOD +
+               dp(i - 1, totA, conL + 1) % MOD) % MOD;
     return mem[i][totA][conL] = ans;
 }
 
 int checkRecord(int n) {
-mem.assign(n,vector<vector<int>>(2,vector<int>(3,-1)));
-return dp(0,0,0,n);
+mem.assign(n+1,vector<vector<int>>(2,vector<int>(3,-1)));
+return dp(n,0,0);
 }
 };
