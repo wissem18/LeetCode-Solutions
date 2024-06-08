d528e3a37f4480c73d417f5c5c03a0bf7132f0c6
wiss <73744299+wissem18@users.noreply.github.com>
2024-06-08 16:03:11 +0100

    Time: 270 ms (6.23%), Space: 135.2 MB (19.15%) - LeetHub

---
+++ 0523-continuous-subarray-sum/0523-continuous-subarray-sum.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+bool checkSubarraySum(vector<int> &nums, int k) {
+    map<int, int> occ;
+    int n = nums.size();
+    int sum = 0;
+    int lst=-1;
+    for (int i = 0; i < n; ++i) {
+        sum += nums[i];
+        int x = sum % k;
+        if (x == 0 && i > 0) {
+            return true;
+        }
+        if (occ[x]>1||(occ[x]==1&&lst!=x))
+            return true;
+        occ[x]++;
+        lst=x;  
+    }
+    return false;
+}
+};
