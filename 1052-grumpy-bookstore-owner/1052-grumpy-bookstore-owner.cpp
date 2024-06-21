b9ad1ed38db6343e6860a6b9532cf8eda2fe4993
wiss <73744299+wissem18@users.noreply.github.com>
2024-06-21 14:31:16 +0100

    Time: 31 ms (21.86%), Space: 34.2 MB (82.09%) - LeetHub

--- 1052-grumpy-bookstore-owner/1052-grumpy-bookstore-owner.cpp
+++ 1052-grumpy-bookstore-owner/1052-grumpy-bookstore-owner.cpp
@@ -1,28 +1,25 @@
 class Solution {
 public:
-int n;
-vector<vector<int>> mem;
-
-int dp(int i, int x, vector<int> &customers, vector<int> &grumpy, int &minutes) {
-    if (i >= n)
-        return 0;
-    
-    if(mem[i][x]!=-1)
-        return mem[i][x];
-    int ans = dp(i + 1, x, customers, grumpy, minutes) + (grumpy[i] ? 0 : customers[i]);
-    if (!x) {
-        int cum = 0;
-        for (int j = i; j < min(n, i + minutes); ++j) {
-            cum += customers[j];
-        }
-        ans = max(ans, dp(i + minutes, 1, customers, grumpy, minutes) + cum);
-    }
-    return mem[i][x]=ans;
-}
-
 int maxSatisfied(vector<int> &customers, vector<int> &grumpy, int minutes) {
-    n = customers.size();
-    mem.assign(n,vector<int>(2,-1));
-    return dp(0,0,customers,grumpy,minutes);
+    int n = customers.size();
+    int ans=0;
+    for (int i = 0; i < n; ++i) {
+        if(!grumpy[i])
+            ans+=customers[i];
+    }
+    int cur=0;
+    for (int i = 0; i < minutes; ++i) {
+        if(grumpy[i])
+            cur+=customers[i];
+    }
+    int maxProfit=cur;
+    for (int i = minutes; i <n ; ++i) {
+        if(grumpy[i])
+            cur+=customers[i];
+        if(grumpy[i-minutes])
+            cur-=customers[i-minutes];
+        maxProfit=max(maxProfit,cur);
+    }
+    return ans+maxProfit;
 }
 };
