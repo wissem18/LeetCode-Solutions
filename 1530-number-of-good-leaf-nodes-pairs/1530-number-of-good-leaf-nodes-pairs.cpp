eee693e5fb688df8c2b285ea0405ca6b3204e7d2
wiss <73744299+wissem18@users.noreply.github.com>
2024-07-18 17:05:44 +0100

    Time: 100 ms (18.09%), Space: 44.3 MB (17.78%) - LeetHub

---
+++ 1530-number-of-good-leaf-nodes-pairs/1530-number-of-good-leaf-nodes-pairs.cpp
@@ -0,0 +1,58 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+map<TreeNode *, vector<TreeNode *>> adj;
+unordered_set<TreeNode *> leaves;
+
+void helper(TreeNode *root) {
+    if (!root)
+        return;
+    if (!root->left && !root->right)
+        leaves.insert(root);
+    if (root->left) {
+        adj[root].push_back(root->left);
+        adj[root->left].push_back(root);
+        helper(root->left);
+    }
+
+    if (root->right) {
+        adj[root].push_back(root->right);
+        adj[root->right].push_back(root);
+        helper(root->right);
+    }
+
+}
+
+int ans = 0;
+
+void dfs(TreeNode *root, TreeNode *par, int distance) {
+    if (distance == 0)
+        return;
+    for (auto node: adj[root]) {
+        if (node == par)
+            continue;
+        if (leaves.count(node))
+            ans++;
+        dfs(node, root, distance - 1);
+    }
+}
+
+int countPairs(TreeNode *root, int distance) {
+    helper(root);
+    for (auto leaf: leaves) {
+        dfs(leaf, nullptr, distance);
+    }
+    ans /= 2;
+    return ans;
+}
+};
