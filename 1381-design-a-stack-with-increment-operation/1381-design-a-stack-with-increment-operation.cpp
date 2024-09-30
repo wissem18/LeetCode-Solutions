bb6856b5ea3613be44fc5122fac575f8d499820a
wiss <73744299+wissem18@users.noreply.github.com>
2024-10-01 00:05:19 +0100

    Time: 24 ms (80.19%), Space: 26.1 MB (29.97%) - LeetHub

--- 1381-design-a-stack-with-increment-operation/1381-design-a-stack-with-increment-operation.cpp
+++ 1381-design-a-stack-with-increment-operation/1381-design-a-stack-with-increment-operation.cpp
@@ -2,27 +2,39 @@ class CustomStack {
 public:
     int mxSize;
     vector<int> stack;
+    vector<int> inc;
+    int lst;//index of last element which is the top of stack
 
     CustomStack(int maxSize) {
         mxSize = maxSize;
+        inc.resize(maxSize);
+        lst = -1;
     }
 
     void push(int x) {
         if (stack.size() == mxSize)return;
         stack.push_back(x);
+        lst++;
     }
 
     int pop() {
         if (stack.empty())
             return -1;
         int ans = stack.back();
+        int increments = inc[lst];
+        ans += increments;
+        inc[lst] = 0;
+        lst--;
+        if (lst >= 0)
+            inc[lst] += increments;
         stack.pop_back();
         return ans;
     }
 
     void increment(int k, int val) {
-        for (int i = 0; i < min(k, int(stack.size())); ++i) {
-            stack[i] += val;
+        if (lst != -1) {
+            int ind = min(k - 1, lst);
+            inc[ind] += val;
         }
     }
 };
