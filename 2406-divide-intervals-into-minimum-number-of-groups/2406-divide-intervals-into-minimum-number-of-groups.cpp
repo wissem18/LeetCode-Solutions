1110139527c3d3f01788cb11595ef8ac1f98f5d4
wiss <73744299+wissem18@users.noreply.github.com>
2024-10-12 21:03:54 +0100

    Time: 370 ms (36.12%), Space: 226.7 MB (13.26%) - LeetHub

--- 2406-divide-intervals-into-minimum-number-of-groups/2406-divide-intervals-into-minimum-number-of-groups.cpp
+++ 2406-divide-intervals-into-minimum-number-of-groups/2406-divide-intervals-into-minimum-number-of-groups.cpp
@@ -1,12 +1,16 @@
 class Solution {
 public:
 int minGroups(vector<vector<int>> &intervals) {
-    vector<int> points(1e6 + 2, 0);
+    int mx = 0;
+    for (int i = 0; i < intervals.size(); ++i) {
+        mx = max(mx, intervals[i][1]);
+    }
+    vector<int> points(mx + 2, 0);
     for (auto interval: intervals) {
         points[interval[0]]++;
         points[interval[1] + 1]--;
     }
-    for (int i = 1; i <= 1e6; ++i) {
+    for (int i = 1; i <= mx; ++i) {
         points[i] += points[i - 1];
     }
     int ans = *max_element(points.begin(), points.end());
