e28d7fa89764a67dd56af353efcdbb81b0b15e0d
wiss <73744299+wissem18@users.noreply.github.com>
2024-10-17 21:40:44 +0100

    Time: 0 ms (100.00%), Space: 7.8 MB (12.41%) - LeetHub

---
+++ 0670-maximum-swap/0670-maximum-swap.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+int maximumSwap(int num) {
+    int ans = num;
+    string s = to_string(num);
+    for (int i = 0; i < s.size(); ++i) {
+        for (int j = i + 1; j < s.size(); ++j) {
+            swap(s[i], s[j]);
+            ans = max(ans, stoi(s));
+            swap(s[i], s[j]);
+        }
+    }
+    return ans;
+}
+};
